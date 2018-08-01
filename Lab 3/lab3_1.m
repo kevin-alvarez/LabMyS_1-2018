2c1315b8aa021fdc4367fe63e59366d650162db1
Gerardo Zúñiga <monodeth@gmail.com>
2018-07-31 21:16:44 -0400

    Update part 1

--- Lab 3/lab3_1.m
+++ Lab 3/lab3_1.m
@@ -1,41 +1,52 @@
 % Primera parte Laboratorio 3
 numc = [3 10]; % numerador de la funcion de transferencia
 denc = [25 -4 30]; % denominador de la funcion de transferencia
+H = tf(numc,denc);
+
+figure('Position', [100 100 800 500]);
+step(H); % respuesta al escalon
+grid;
+title('Respuesta al escalon');
 
 % Se grafica la respuesta continua al escalon
-figure('Position', [100 100 800 600]);
-step(numc, denc); % respuesta al escalon
+figure('Position', [100 100 800 500]);
+step(H); % respuesta al escalon
 grid;
 title('Respuesta al escalon (continuo)');
+xlim([0 15]);
+ylim([-1.2 1.5]);
 
 % Se discretiza la funcion con un bloqueador de orden zero
 T1 = 0.1; % tiempo de muestreo
-[numd1, dend1] = c2dm(numc, denc, T1, 'zoh'); % discretizacion
-[y1] = dstep(numd1, dend1, 200+1);
-x1 = 0:T1:T1*200;
+H1 = c2d(H, T1, 'zoh'); % discretizacion
+
 figure('Position', [100 100 800 600]);
 subplot(2, 1, 1);
-stairs(x1, y1);
+step(H1);
 grid;
-title('Respuesta discretizada con T = ');
-xlim([0 10]);
-ylim([-1.5 1.5]);
+title(strcat('Respuesta discretizada con T = ',num2str(T1)));
+xlim([0 20]);
+ylim([-1.2 1.5]);
+
 
 T2 = 0.05; % tiempo de muestreo
-[numd2, dend2] = c2dm(numc, denc, T2, 'zoh'); % discretizacion
-[y2] = dstep(numd2, dend2, 200+1);
-x2 = 0:T2:T2*200;
+H2 = c2d(H, T2, 'zoh'); % discretizacion
+
 subplot(2, 1, 2);
-stairs(x2, y2);
+step(H2);
 grid;
-title('Respuesta discretizada con T = ');
-xlim([0 10]);
-ylim([-1.5 1.5]);
+title(strcat('Respuesta discretizada con T = ',num2str(T2)));
+xlim([0 20]);
+ylim([-1.2 1.5]);
+
 
 % Se transforma la funcion de discreta a continua
 Tc = 0.1;
-[numcont, dencont] = d2cm(numd1, dend1, Tc, 'zoh'); % a continua
-figure('Position', [100 100 800 600]);
-step(numcont, dencont);
+[num, den] = tfdata(H1); 
+[numdc, dendc] = d2cm(num,den, Tc, 'zoh'); % a continua
+figure('Position', [100 100 800 500]);
+step(numdc, dendc);
 grid;
 title('Funcion transformada de discreta a continua');
+xlim([0 20]);
+ylim([-1.2 1.5]);
