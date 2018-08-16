b1813a30176073349098944507b8635c89a120e0
Gerardo Zúñiga <monodeth@gmail.com>
2018-08-16 03:52:22 -0300

    Lab 4, segunda parte

---
+++ Lab 4/lab4_2_1.m
@@ -0,0 +1,38 @@
+% Laboratorio 4 - Parte 2.1
+
+% Ejemplos del sistema modelado
+graficarSistema(10,1.5,1,2);
+graficarSistema(5,2,1,2);
+graficarSistema(5,1.5,1,10);
+
+
+% Funcion resolutora del sistema de estanques
+function [ F2, dh ] = sistema(F1, A, h1_0, h2_0)
+    % Construir y resolver Matrices
+    dh = [0, 0; 0, -1/A]*[h1_0;h2_0]+[0;1/A]*F1;
+    F2 = [0, 1]*[h1_0; h2_0];
+end
+
+% Esta función grafica el comportamiento de un sistema desde un estado
+% inicial
+function  graficarSistema(F1, A, h1, h2) 
+    h1_0 = h1;
+    h2_0 = h2;
+    y = [];
+    for i = 1:25
+        [F, dh] = sistema(F1, A, h1, h2) ;
+        y = [y, F];
+        h = [h1,h2]'+dh';
+        h1 = h(1,1);
+        h2 = h(1,2);
+    end
+    
+    figure('Position', [100 100 800 500]);
+    plot(y)
+    grid;
+    xlabel('Tiempo');
+    ylabel('F2');
+    title(sprintf('Sistema de estanques con F1=%d A=%.1f h1=%d h2=%d', F1, A, h1_0, h2_0));
+
+end
+
---
+++ Lab 4/lab4_2_2.m
@@ -0,0 +1,37 @@
+% Laboratorio 4 - Parte 2.2
+
+% Ejemplo del sistema modelado
+
+graficarSistema(5,10, 50, 2, 1, 0,0);
+
+
+% Funcion resolutora del sistema de estanques
+function [ Y, dV ] = sistema(Vs, R1, R2, C1, C2, V1, V2)
+    % Construir y resolver Matrices
+    dV = [-1/(R1*C1), 0; 0,-1/(R2*C2)]*[V1;V2]+[1/(R1*C1);1/(R2*C2)]*Vs;
+    Y = [1, 1]*[V1; V2];
+end
+
+% Esta función grafica el comportamiento de un sistema desde un estado
+% inicial
+function  graficarSistema(Vs, R1, R2, C1, C2, V1, V2) 
+    V1_0 = V1;
+    V2_0 = V2;
+    y = [];
+    for i = 1:150
+        [Y, dV] = sistema(Vs, R1, R2, C1, C2, V1, V2) ;
+        y = [y, Y];
+        V = [V1,V2]'+dV';
+        V1 = V(1,1);
+        V2 = V(1,2);
+    end
+    
+    figure('Position', [100 100 800 500]);
+    plot(y)
+    grid;
+    xlabel('Tiempo');
+    ylabel('V1+V2');
+    title('Sistema electrico');
+
+end
+
